a6ee33165ee25cbd93eb0b81691d0216a2a54928
adwardlee <4065156@qq.com>
2020-07-09 10:31:10 +0800

    Create 039_Combination_Sum.py

---
+++ 039_Combination_Sum.py
@@ -0,0 +1,56 @@
+'''
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+The same repeated number may be chosen from candidates unlimited number of times.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
+Example 2:
+
+Input: candidates = [2,3,5], target = 8,
+A solution set is:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+'''
+
+
+class Solution:
+    def combinationSum(self, candidates, target: int):
+        self.output = []
+        self.candidates = sorted(candidates)
+        self.length = len(self.candidates)
+        self.dfs(0, [], target, 0)
+        return self.output
+
+    def dfs(self, sums, nums, target, index):
+        if sums == target:
+            self.output.append(nums.copy())
+            return True
+        if sums > target:
+            return False
+        count = index
+        for x in self.candidates[index:]:
+            sums += x
+            nums.append(x)
+            if self.dfs(sums, nums, target, count):
+                sums -= x
+                nums.pop()
+                break
+            else:
+                sums -= x
+                nums.pop()
+            count += 1
+        return False
